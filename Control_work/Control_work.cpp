aab6f6078758bbe6e1fbbe866fc1f66f3036f48f
Anton-maxi <zborikanton@gmail.com>
2026-06-04 16:50:35 +0200

    Add project files.

---
+++ Control_work/Control_work.cpp
@@ -0,0 +1,9 @@
+﻿// Control_work.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
